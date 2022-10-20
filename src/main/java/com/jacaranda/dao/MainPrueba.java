654d8a05ef5c5cb88aad6b88f3085e662ae7cfd8
chisela14 <chisela14@gmail.com>
2022-10-20 10:13:28 +0200

    prueba

--- src/main/java/com/jacaranda/dao/MainPrueba.java
+++ src/main/java/com/jacaranda/dao/MainPrueba.java
@@ -1,20 +1,33 @@
 package com.jacaranda.dao;
 
+import com.jacaranda.model.UserException;
+
 public class MainPrueba {
 
 
 	public static void main(String[] args) {
 		String code = "ChiselaC";
-		String key = "adm1";
+		// String key = "adm1";
 		DaoUser du = new DaoUser();
 		
+//		try {
+//			boolean b = du.userIsValid(code, key);
+//			System.out.println(b);
+//		}catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+		
 		try {
-			boolean b = du.userIsValid(code, key);
-			System.out.println(b);
-		}catch(Exception e) {
+			du.getUser(code);
+		} catch (UserException | DaoException e) {
 			System.out.println(e.getMessage());
 		}
 		
+		try {
+			du.getUsers();
+		} catch (UserException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
